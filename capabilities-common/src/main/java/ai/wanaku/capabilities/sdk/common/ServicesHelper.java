b777eb91f63a88b7a4058c52d4329d35ffd5cf6b
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-07-10 08:57:34 +0200

    Added Javadoc to the capabilities-common module

--- capabilities-common/src/main/java/ai/wanaku/capabilities/sdk/common/ServicesHelper.java
+++ capabilities-common/src/main/java/ai/wanaku/capabilities/sdk/common/ServicesHelper.java
@@ -2,7 +2,17 @@ package ai.wanaku.capabilities.sdk.common;
 
 import java.io.File;
 
+/**
+ * Helper class for common service-related utilities.
+ */
 public class ServicesHelper {
+    /**
+     * Returns the canonical home directory for a given service name.
+     * This is typically the user's home directory followed by the service name.
+     *
+     * @param name The name of the service.
+     * @return The canonical home directory path for the service.
+     */
     public static String getCanonicalServiceHome(String name) {
         return System.getProperty("user.home") + File.separator + name;
     }
