0f49d2cd37e0243ad556e272fd10014287b8740b
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-07-10 09:13:17 +0200

    Added Javadoc to the capabilities-exchange module

--- capabilities-exchange/src/main/java/ai/wanaku/capabilities/sdk/util/ProvisioningHelper.java
+++ capabilities-exchange/src/main/java/ai/wanaku/capabilities/sdk/util/ProvisioningHelper.java
@@ -7,10 +7,20 @@ import ai.wanaku.core.exchange.ProvisionRequest;
 import ai.wanaku.core.exchange.Secret;
 import java.net.URI;
 
+/**
+ * Helper class for provisioning configurations and secrets.
+ */
 public final class ProvisioningHelper {
 
     private ProvisioningHelper() {}
 
+    /**
+     * Provisions a configuration and a secret based on the provided request and provisioner.
+     *
+     * @param request The {@link ProvisionRequest} containing the configuration and secret payloads.
+     * @param provisioner The {@link ConfigProvisioner} used to provision the configuration and secret.
+     * @return A {@link ProvisionedConfig} containing the URIs of the provisioned configuration and secret.
+     */
     public static ProvisionedConfig provision(ProvisionRequest request, ConfigProvisioner provisioner) {
         final Configuration configuration = request.getConfiguration();
         final Secret secret = request.getSecret();
