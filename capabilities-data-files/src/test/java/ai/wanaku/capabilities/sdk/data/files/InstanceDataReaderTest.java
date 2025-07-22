9a0c8fc42d805094afe6dca5d266c6ece203ab06
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-07-22 16:23:49 +0200

    Flaky on Windows and needs to be investigated

--- capabilities-data-files/src/test/java/ai/wanaku/capabilities/sdk/data/files/InstanceDataReaderTest.java
+++ capabilities-data-files/src/test/java/ai/wanaku/capabilities/sdk/data/files/InstanceDataReaderTest.java
@@ -7,12 +7,15 @@ import java.nio.file.Path;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisabledOnOs(OS.WINDOWS)
 class InstanceDataReaderTest {
 
     @TempDir
