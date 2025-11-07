77d87f5091fdacc9738c2abc8afaca463c136041
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-11-07 13:50:08 +0100

    Added support for retrieving the data from a data store
    Added support for running callbacks after registration, ping and deregistration
    
    Ref: wanaku-ai/wanaku#637

---
+++ capabilities-discovery/src/main/java/ai/wanaku/capabilities/sdk/discovery/DiscoveryLogCallback.java
@@ -0,0 +1,32 @@
+package ai.wanaku.capabilities.sdk.discovery;
+
+import ai.wanaku.api.discovery.DiscoveryCallback;
+import ai.wanaku.api.discovery.RegistrationManager;
+import ai.wanaku.api.types.providers.ServiceTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class DiscoveryLogCallback implements DiscoveryCallback {
+    private static final Logger LOG = LoggerFactory.getLogger(DiscoveryLogCallback.class);
+
+    @Override
+    public void onPing(RegistrationManager manager, ServiceTarget target, int status) {
+        if (status != 200) {
+            LOG.warn("Pinging router failed with status {}", status);
+        } else {
+            LOG.trace("Pinging router completed successfully");
+        }
+    }
+
+    @Override
+    public void onRegistration(RegistrationManager manager, ServiceTarget target) {
+        LOG.debug("The service {} successfully registered with ID {}.", target.getService(), target.getId());
+    }
+
+    @Override
+    public void onDeregistration(RegistrationManager manager, ServiceTarget target, int status) {
+        if (status != 200) {
+            LOG.warn("De-registering service {} failed with status {}", target.getServiceType().asValue(), status);
+        }
+    }
+}
