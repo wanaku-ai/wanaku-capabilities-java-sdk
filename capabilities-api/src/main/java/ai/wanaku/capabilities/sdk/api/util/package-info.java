c42b9776db9cf25cb9b15269b88f005da30dc569
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-02-09 12:38:31 +0100

    Added missing package info in the API module

---
+++ capabilities-api/src/main/java/ai/wanaku/capabilities/sdk/api/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Utility classes for the Wanaku Capabilities SDK.
+ * <p>
+ * This package contains utility classes and constants used throughout the SDK,
+ * including reserved names for special argument handling in the tool invocation protocol.
+ */
+package ai.wanaku.capabilities.sdk.api.util;
